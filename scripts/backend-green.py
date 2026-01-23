798cd8e25a9de3f0e33f519ee29a4d27efacc240
udaykiriti <udaykiriti624@gmail.com>
2026-01-23 13:58:50 +0530

    Modified py scripts with changes in fun names and delay resp

--- scripts/backend-green.py
+++ scripts/backend-green.py
@@ -3,7 +3,7 @@ from http.server import BaseHTTPRequestHandler, HTTPServer
 
 port = int(sys.argv[1]) if len(sys.argv) > 1 else 8084
 
-class SimpleHandler(BaseHTTPRequestHandler):
+class _handler(BaseHTTPRequestHandler):
     def do_GET(self):
         self.send_response(200)
         self.send_header('Content-type', 'text/plain')
@@ -12,8 +12,7 @@ class SimpleHandler(BaseHTTPRequestHandler):
         response = f"Hello from the GREEN Environment running on PORT {port}!\n"
         self.wfile.write(response.encode())
 
-server_address = ('', port)
-httpd = HTTPServer(server_address, SimpleHandler)
+server_addr = ('', port)
+httpd = HTTPServer(server_addr, _handler)
 print(f"Green Backend running on port {port}...")
 httpd.serve_forever()
-
