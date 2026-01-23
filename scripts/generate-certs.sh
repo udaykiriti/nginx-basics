c3d59ad19b6c247156af5cbe772cf3e3ab5a33f5
udaykiriti <udaykiriti624@gmail.com>
2026-01-23 13:56:10 +0530

    Modification : Added some colors and Enhcmts

--- scripts/generate-certs.sh
+++ scripts/generate-certs.sh
@@ -1,11 +1,18 @@
 #!/bin/bash
 
+# colors
+GREEN='\033[0;32m'
+BLUE='\033[0;34m'
+RED='\033[0;31m'
+NC='\033[0m'
+BRIGHT='\033[1m'
+
 mkdir -p certs
 
-echo "Generating Self-Signed Certificate..."
+echo "${GREEN}[Start]: Generating Self-Signed Certificate...${NC}"
 openssl req -x509 -nodes -days 365 -newkey rsa:2048 \
     -keyout certs/nginx-selfsigned.key \
     -out certs/nginx-selfsigned.crt \
     -subj "/C=US/ST=State/L=City/O=Organization/OU=Unit/CN=localhost"
 
-echo "Certificates generated in certs/ directory."
+echo "${GREEN}[Done]: Certificates generated in certs/ directory..${NC}"
